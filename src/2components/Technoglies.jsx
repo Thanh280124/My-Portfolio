07049b5a17e7d2844d4599139226d2a5a31bab97
Thanh <helu0384@gmail.com>
2024-09-14 22:29:09 +0300

    four

--- src/2components/Technoglies.jsx
+++ src/2components/Technoglies.jsx
@@ -10,7 +10,7 @@ function Technoglies() {
       <div className="mt-14">
         <div>
         <div className="flex justify-between items-center">
-          <h2 className="text-lg font-semibold">HTML</h2>
+          <h2 className="text-lg font-semibold">HTML, CSS, JavaScript,React, Node.js</h2>
           <p className="text-gray-500">Advanced </p>
         </div>
         <span className="w-full h-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md mt-2"/>
@@ -18,20 +18,36 @@ function Technoglies() {
 
         <div className="mt-8">
         <div className="flex justify-between items-center">
-          <h2 className="text-lg font-semibold">CSS</h2>
-          <p className="text-gray-500">Advanced </p>
-        </div>
-        <span className="w-full h-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md mt-2"/>
-        </div>
-
-        <div className="mt-8">
-        <div className="flex justify-between items-center">
-          <h2 className="text-lg font-semibold">React</h2>
+          <h2 className="text-lg font-semibold">Appwrite, Express.JS,
+          MongoDB</h2>
           <p className="text-gray-500">Advanced </p>
         </div>
         <span className="w-[75%] h-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md mt-2"/>
         </div>
 
+        <div className="mt-8">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-semibold">ReactJS, React Native, Typescript</h2>
+          <p className="text-gray-500">Advanced </p>
+        </div>
+        <span className="w-[75%] h-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md mt-2"/>
+        </div>
+
+        <div className="mt-8">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-semibold">C sharp , C, Python , Java (Basic)</h2>
+          <p className="text-gray-500">Basic</p>
+        </div>
+        <span className="w-[25%] h-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md mt-2"/>
+        </div>
+
+        <div className="mt-8">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-semibold">Git & Githubt</h2>
+          <p className="text-gray-500">Advanced </p>
+        </div>
+        <span className="w-[85%] h-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md mt-2"/>
+        </div>
         
       </div>
       </div>
