9e1fa856694b48412c66eac1bc0183549ec31482
Thanh <helu0384@gmail.com>
2024-09-14 21:08:33 +0300

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ export default {
   content: [  "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    // screens:{
-    //   ssm:'375px',
-    //   sm:'640px',
-    //   md:'768px',
-    //   lg:'1024px',
-    //   xl:'1700px',
-    // },
+    screens:{
+      ssm:'375px',
+      sm:'640px',
+      md:'768px',
+      lg:'1024px',
+      xl:'1700px',
+    },
     extend: {
       translate: {
         '1/3': '33.3333%',  // Adds translate-x-1/3 and translate-y-1/3
