5abcb0537c53cb02e382f4e7e074521e8239cf7e
Thanh <helu0384@gmail.com>
2024-11-11 11:24:58 +0200

    change pic

--- src/2components/Projects.jsx
+++ src/2components/Projects.jsx
@@ -1,6 +1,8 @@
 import hero from '../assets/hero.jpg';
 import RockPapperScissor from '../assets/RockPaperScisssors.png'
 import NoteApp from '../assets/NoteApp.png'
+import Video from '../assets/AI-Generator.mp4'
+import Aipic from '../assets/AiPic.jpg'
 function Projects() {
   return (   
          <section id='projects'>
@@ -18,12 +20,12 @@ function Projects() {
               </div>
             </div>
             <div className='border rounded-md p-5 border-gray-500 flex-1'>
-              <img src={hero} alt="" className='w-full h-auto' />
+              <img src={Aipic} alt="" className='w-full h-[350px]' />
               <h3 className='text-2xl font-semibold mt-8'>AI-Video-Generator</h3>
               <p className='text-gray-400 text-sm mt-2'>A mobile application developed using React Native and TypeScript. This app leverages Appwrite for user account management and facilitates video uploads and image storage. It allows users to generate videos using AI and manage their multimedia content seamlessly.</p>
 
               <div className='flex  mt-12 gap-3'>
-                <button className='flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 to-cyan-500 border rounded-3xl hover:from-blue-700 hover:to-cyan-700 '><a target='_blank' href="https://thanh280124.github.io/TodoList/">Live Preview</a></button>
+                <button className='flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 to-cyan-500 border rounded-3xl hover:from-blue-700 hover:to-cyan-700 '><a target='_blank' href={Video}>Live Preview</a></button>
                 <button className='flex-1 text-sm py-3  border rounded-3xl hover:border-blue-500 hover:text-blue-500'><a target='_blank' href="https://github.com/Thanh280124/AI-Video-Generater">View Github</a></button>
               </div>
             </div>
